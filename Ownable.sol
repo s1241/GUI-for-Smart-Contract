ba45f9b3e2fe009803ebac740b169b77cace63ee
s1241 <37220425+s1241@users.noreply.github.com>
2018-05-07 11:09:39 +0200

    Add files via upload

---
+++ Ownable.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.4.15;
+
+contract Ownable {
+
+  // Owner of the contract
+  address public owner;
+
+  event ChangementOwnership(address indexed _by, address indexed _to);
+
+  function Ownable() {
+    owner = msg.sender;
+  }
+
+  /// @dev Reverts if called by any account other than the owner.
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+  function transferOwnership(address newOwner) external onlyOwner {
+    require(newOwner != address(0));
+    owner = newOwner;
+
+    ChangementOwnership(msg.sender, newOwner);
+  }
+
+}
